3feb5d4e791c1659d3e01bfd8aa323caa1fa4e34
MochiKoffe <sophiem47@nycstudents.net>
2026-02-12 10:13:12 -0500

    day 12

--- heuih.py
+++ heuih.py
@@ -1,6 +1,19 @@
-item = {
-    "name": "Samsung 55\" 4K UHD TV",
-    "price": 429.99,
-    "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-}
+solarballs = [ 
+{
+    "name": "Benus",
+    "price": 42.670,
+    "department": "planet",
+    "description": "VENUSS PLANET OF LOVEEEE WAS DESTROYED BY GLOBAL WARMINGGGGGGG GHAIUEHGUIEH I LOVE VENUS"
+}, 
+{
+    "name": "SUNNNN",
+    "price": 718973589759189,
+    "department": "star",
+    "description": "THE CENTER OF OUR SOLAR SYSTEM SUNNNNMY BABYYYYY"
+},
+
+]
+
+
+print(solarballs[0]["name"])
+print(solarballs[1]["description"])
