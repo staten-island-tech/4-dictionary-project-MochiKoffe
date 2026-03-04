25938ec33056b1e64c9f563a074feac3239e2162
MochiKoffe <sophiem47@nycstudents.net>
2026-03-04 10:22:12 -0500

    day 16

--- exampracticeAAAAA.py
+++ exampracticeAAAAA.py
@@ -1,14 +1,14 @@
-""" def occupied (n,y,t):
+def occupied (n,y,t):
     found = 0
     for i in range(n):
         if y[i] == "C" and t[i] == "C":
             found += 1
     print(found)
-occupied(5,"CCC..","C..CC") """
+occupied(5,"CCC..","C..CC")
 
 
 
-""" def language (sentence):
+def language (sentence):
     t=0
     T=0
     s=0
@@ -28,10 +28,10 @@ occupied(5,"CCC..","C..CC") """
     if s+S >= t+T:
         print("probably French")
 language("The red cat sat on the mat. Why are you so sad cat? Don't ask that.")
-language("The redsssssssssssssssss") """
+language("The redsssssssssssssssss")
 
 
-def honi (word): 
+""" def honi (word): 
     H=0
     O=0
     N=0
@@ -41,16 +41,16 @@ def honi (word):
     for i in range (len(word)):
         if word[i] == current:
             H += 1
-        current == "O"
+            current == "O"
         if word[i] == current:
             O += 1
-        current == "N"
+            current == "N"
         if word[i] == current: 
             N += 1
-        current == "I"
+            current == "I"
         if word[i] == current:
             I += 1
-        if H+O+N+I == 4:
+        if H and O and N and I == 1:
             HONI += 1
             H=0
             O=0
@@ -60,9 +60,30 @@ def honi (word):
         else: 
             HONI == 0
     print(HONI)
+honi("HONI")
 honi("PROHODNIHODNIK")
 honi("HHHHOOOONNNNIIII")
-honi("MAGNUS")
+honi("MAGNUS") """
+
+
+def magnus(word):
+    count = 0
+    state = 0
+
+    for char in word: 
+        if state == 0 and char.upper() == "H":
+            state = 1
+        elif state == 1 and char.upper() == "O":
+            state = 2
+        elif state == 2 and char.upper() == "N":
+            state = 3
+        elif state == 3 and char.upper() == "I":
+            state = 0
+            count += 1
+    print(count)
+magnus("HHHHOOOONNNNIIII")
+magnus("MAGNUS")
+magnus("PROHODNIHODNIK")
 
 """ def add (x,y):
     return x + y
